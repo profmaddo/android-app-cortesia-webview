acbcc8576ea5556da6995d971041942477b13dd4
Professor Marco Maddo <profmaddo@gmail.com>
2021-04-18 20:16:32 -0400

    First release
    First release

---
+++ AppCortesiaWebView/app/src/main/java/br/com/modelo/app/MainActivity.java
@@ -0,0 +1,29 @@
+package br.com.modelo.app;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+/**
+ * Cortesia, professor Marco Maddo
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @SuppressLint("SetJavaScriptEnabled")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        WebView webView = findViewById(R.id.webview);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+        webView.loadUrl("https://www.marcomaddo.com.br");
+
+    }
+}
