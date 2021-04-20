df817a443562199bad61b3b43aa16065c8c1a306
Miguel Rosa <miguelrosa.programacao@gmail.com>
2021-04-20 09:51:29 +0100

    Update MainActivity.java

--- AppCortesiaWebView/app/src/main/java/br/com/modelo/app/MainActivity.java
+++ AppCortesiaWebView/app/src/main/java/br/com/modelo/app/MainActivity.java
@@ -25,5 +25,6 @@ public class MainActivity extends AppCompatActivity {
 
         webView.loadUrl("https://www.marcomaddo.com.br");
 
+        webView.setWebViewClient(new WebViewClient());
     }
 }
